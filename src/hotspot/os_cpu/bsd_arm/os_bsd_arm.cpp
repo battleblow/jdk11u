ddafc42a3c6e941d56e633d21be1f0db5da29c61
Greg Lewis <glewis@eyesbeyond.com>
2023-01-20 06:03:22 -0800

    Sync with Linux

--- src/hotspot/os_cpu/bsd_arm/os_bsd_arm.cpp
+++ src/hotspot/os_cpu/bsd_arm/os_bsd_arm.cpp
@@ -315,7 +315,7 @@ extern "C" int JVM_handle_bsd_signal(int sig, siginfo_t* info,
   }
 
   // Handle SafeFetch faults:
-  if (uc != NULL) {
+  if ((sig == SIGSEGV || sig == SIGBUS) && uc != NULL) {
     address const pc = (address) os::Bsd::ucontext_get_pc(uc);
     if (pc && StubRoutines::is_safefetch_fault(pc)) {
       os::Bsd::ucontext_set_pc(uc, StubRoutines::continuation_for_safefetch_fault(pc));
